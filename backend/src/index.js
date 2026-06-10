6b9a11da8a24622da4da83e884168105face339b
varunk-2005 <varun.koficial@gmail.com>
2026-06-11 01:27:49 +0530

    fixing errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,11 @@ dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173","chat-app-nine-zeta-90.vercel.app"],
+  origin: [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+    "https://chat-app-nine-zeta-90.vercel.app" 
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
