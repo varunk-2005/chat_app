24ef5de2cfb9a68594c6d6e9be9f9f3212bce9e5
varunk-2005 <varun.koficial@gmail.com>
2026-06-12 16:31:12 +0530

    Add request interceptor to axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,12 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: 'https://chat-backend-uv9f.onrender.com/api',
-  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
